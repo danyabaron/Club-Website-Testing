cec4837ee2bfeae080197988c76e2c65d947629e
Josh <premiumant56@gmail.com>
2025-03-01 14:52:57 -0500

    Added the React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './index.css'
-import Home from './Components/Home.jsx'
-import Navbar from './Components/Navbar.jsx'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      {/* load navbar */}
+    <Router>
       <Navbar />
-      {/* load home page */}
-      <Home />
-    </div>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
