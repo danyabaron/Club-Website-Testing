9a71b7a9b69ec0391655924922377113b4deca7b
chickenjenders <tylerjenny8@gmail.com>
2025-03-02 22:51:12 -0500

    comm forum, footer, and pictures for events page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <section>
+      < footer className="bg-black text-gray-400 py-4" >
+        <div className="max-w-6xl mx-auto px-4 text-center">
+          <p>&copy; 2025 Cyber Club. All Rights Reserved.</p>
+        </div>
+      </footer >
+    </section>
+  );
+};
+
+export default Footer;
