4a8c9c45c2def1f98b871be78953b3d0c2005aeb
NatureBoyJsou <100043411+NatureBoyJsou@users.noreply.github.com>
2025-03-11 17:32:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], // Ensure React plugin is used
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['4fd2-68-205-76-76.ngrok-free.app'], // Allow this host
     proxy: {
       '/api': {
         target: 'http://localhost:5173',
