a4b4cfbace1ef4e2ae53e56128e9d568c0c29369
NatureBoyJsou <100043411+NatureBoyJsou@users.noreply.github.com>
2025-03-12 11:47:42 -0400

    Instagram Links & Remote Access

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['4fd2-68-205-76-76.ngrok-free.app'], // Allow this host
+    allowedHosts: ['72a8-68-205-76-76.ngrok-free.app'], // Allow this host
     proxy: {
       '/api': {
         target: 'http://localhost:5173',
